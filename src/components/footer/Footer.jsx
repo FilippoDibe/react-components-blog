2b4c94413520ef1638e811331c78eeba9578da26
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2024-06-12 16:43:39 +0200

    add footer style

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,1 +1,13 @@
-import ""
+import  './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footerContainer">
+            <p className="footerText">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto aut alias sapiente quos quia. Eos omnis necessitatibus quia, voluptates cumque reprehenderit unde quod, exercitationem ipsa pariatur corrupti dolore. Eius, similique.
+            </p>
+        </footer>
+    );
+}
+
+export default Footer
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,13 @@
 import "./Main.css";
 import CardComponent from '../cards/Card';
+import Footer from "../Footer/Footer";
 const Main = () => {
     return (
         <main className="background">
             <div className="container">
                 <h1 className="title">Il Mio Blog</h1>
                 <CardComponent/>
+                <Footer/>
             </div>
 
         </main>
